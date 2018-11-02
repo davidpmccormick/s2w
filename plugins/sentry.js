f57b50aa2b94deb7228cc68fa16dfdead75f096a
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-02 17:08:10 +0000

    Use 'release' instead of 'version'

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -4,7 +4,7 @@ import * as Sentry from '@sentry/browser';
 export default (({ env }) => {
   Sentry.init({
     dsn: 'https://cbf37ea76a9f4f0883c7fb2fb304ebce@sentry.io/1309056',
-    version: env.sha
+    release: env.sha
   });
 
   Vue.prototype.$sentry = Sentry;
