ca19756c926935cfbc1b492fbf9cfdc083cf6d44
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-03 13:57:27 +0000

    Update toggles

--- pages/experiments.vue
+++ pages/experiments.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container">
     <h1>Experiments</h1>
-    <ToggleSwitch v-model="answer" />
+    <div
+      v-for="toggleExperiment in toggleExperiments"
+      :key="toggleExperiment.name">
+      <ToggleSwitch v-model="toggleExperiment.value" />
+    </div>
   </div>
 </template>
 
@@ -13,11 +17,36 @@ export default {
     ToggleSwitch
   },
   async asyncData({ app }) {
-    const { data } = await app.$axios.get('https://yesno.wtf/api');
+    const { data } = await app.$axios.get('https://somewheretowear.com/experiments.json');
+    const { experiments } = data;
+    const tes = experiments.filter(e => e.type === 'toggle');
+    const cookies = [{name: 'toggleTest', value: true}]; // TODO: get cookies from a plugin
+
+    const toggleExperiments = tes.map(te => {
+      const existingCookie = cookies.find(c => c.name === te.name);
+
+      return {...te, value: existingCookie ? existingCookie.value : false };
+    });
 
     return {
-      answer: data.answer === 'yes'
+      toggleExperiments,
     };
+  },
+  watch: {
+    toggleExperiments: {
+      handler(value){
+        const updatedCookieValue = value.map(({name, type, value}) => {
+          return {
+            name,
+            type,
+            value
+          };
+        });
+
+        // TODO: update cookie values here
+      },
+      deep: true
+    }
   }
 };
 </script>
