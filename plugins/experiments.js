8ca00aec5bb76e5970d96df5b27f5b6b9d7c2fb4
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-09 14:05:53 +0000

    Try the right URL

--- plugins/experiments.js
+++ plugins/experiments.js
@@ -1,7 +1,7 @@
 import cookie from 'cookie-cutter';
 
 export default(async ({ store, app }) => {
-  const { data } = await app.$axios.get('https://raw.githubusercontent.com/davidpmccormick/s2w/master/experiments.json');
+  const { data } = await app.$axios.get('https://raw.githubusercontent.com/davidpmccormick/s2w/master/static/experiments.json');
   const { experiments } = data;
   const fetchedToggleExperiments = experiments.filter(e => e.type === 'toggle');
   const maybeCookies = cookie.get('s2w_experiments')  || '[]';
