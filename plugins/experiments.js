3c2da57d308906d914ce3b449eb40ac1ccd85c32
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-09 14:00:28 +0000

    Try raw git JSON

--- plugins/experiments.js
+++ plugins/experiments.js
@@ -1,18 +1,7 @@
 import cookie from 'cookie-cutter';
 
 export default(async ({ store, app }) => {
-  const data = {
-    "experiments": [
-      {
-        "type": "toggle",
-        "name": "toggleTest"
-      },
-      {
-        "type": "ab",
-        "name": "abTest"
-      }
-    ]
-  };
+  const { data } = await app.$axios.get('https://raw.githubusercontent.com/davidpmccormick/s2w/master/experiments.json');
   const { experiments } = data;
   const fetchedToggleExperiments = experiments.filter(e => e.type === 'toggle');
   const maybeCookies = cookie.get('s2w_experiments')  || '[]';
