2f2d2352913e22406b21a5008245919000d429a8
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-09 13:48:46 +0000

    Hard code experiments for now

--- plugins/experiments.js
+++ plugins/experiments.js
@@ -1,7 +1,18 @@
 import cookie from 'cookie-cutter';
 
 export default(async ({ store, app }) => {
-  const { data } = await app.$axios.get('/experiments.json');
+  const data = {
+    "experiments": [
+      {
+        "type": "toggle",
+        "name": "toggleTest"
+      },
+      {
+        "type": "ab",
+        "name": "abTest"
+      }
+    ]
+  };
   const { experiments } = data;
   const fetchedToggleExperiments = experiments.filter(e => e.type === 'toggle');
   const maybeCookies = cookie.get('s2w_experiments')  || '[]';
