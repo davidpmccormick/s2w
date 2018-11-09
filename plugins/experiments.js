5dc1bc9cfd0a15cd8a52877f60bb563134ef07f0
David McCormick <d.mccormick@wellcome.ac.uk>
2018-11-09 13:29:38 +0000

    Use relative path for JSON

--- plugins/experiments.js
+++ plugins/experiments.js
@@ -1,7 +1,7 @@
 import cookie from 'cookie-cutter';
 
 export default(async ({ store, app }) => {
-  const { data } = await app.$axios.get('https://somewheretowear.com/experiments.json');
+  const { data } = await app.$axios.get('/experiments.json');
   const { experiments } = data;
   const fetchedToggleExperiments = experiments.filter(e => e.type === 'toggle');
   const maybeCookies = cookie.get('s2w_experiments')  || '[]';
