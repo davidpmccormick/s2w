210e43136840e172a161100d8c860ccc9309b756
David McCormick <d.mccormick@wellcome.ac.uk>
2018-12-21 12:42:18 +0000

    Stick it in a function, yeah

--- pages/where.vue
+++ pages/where.vue
@@ -3,9 +3,20 @@
     <template slot="h1">Where</template>
     <div class="owl">
       <GmapMap
-        :center="{ lat: -3.350235, lng: 111.995865 }"
+        :center="center"
         :zoom="5"
-        :map-type-id="'terrain'" />
+        :map-type-id="mapTypeId">
+        <GmapMarker
+          v-for="(item, index) in markers"
+          :key="index"
+          :position="item.position"
+          :icon="{
+            url: '/favicon.png',
+            size: {width: 46, height: 46, f: 'px', b: 'px'},
+            scaledSize: {width: 30, height: 30, f: 'px', b: 'px'}
+          }"
+          @click="center=item.position" />
+      </GmapMap>
     </div>
   </BasePage>
 </template>
--- plugins/google-maps.js
+++ plugins/google-maps.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import * as VueGoogleMaps from 'vue2-google-maps';
 
-Vue.use(VueGoogleMaps, {
-  load: { key: 'AIzaSyBWtYlyDa75L_070ne29bC1G-Eo2jHJgLI' }
-});
+export default () => {
+  Vue.use(VueGoogleMaps, {
+    load: { key: 'AIzaSyBWtYlyDa75L_070ne29bC1G-Eo2jHJgLI' }
+  });
+};
